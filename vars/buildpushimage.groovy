82f1cd689d4a42f5d3e8dddbb93201270fd52992
metoo <mohamedmetoo@gmail.com>
2025-08-24 22:31:04 +0300

    new jenkins-script file

--- vars/buildpushimage.groovy
+++ vars/buildpushimage.groovy
@@ -1,4 +1,7 @@
-def call(String image, String tag = "latest") {
+def call(Map args = [:]) {
+    def image = args.image ?: "default-image"
+    def tag = args.tag ?: "latest"
+
     sh "docker build -t ${image}:${tag} ."
     sh "docker push ${image}:${tag}"
 }
