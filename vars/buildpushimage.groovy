f71b17627301efbb8d9168242df4fe11784f33f8
metoo <mohamedmetoo@gmail.com>
2025-08-24 23:22:51 +0300

    new jenkins-script file

--- vars/buildpushimage.groovy
+++ vars/buildpushimage.groovy
@@ -1,17 +1,19 @@
-def call(Map args = [:]) {
-    def image = args.get('image', 'default-image')
-    def tag = args.get('tag', 'latest')
-    def onlyBuild = args.get('onlyBuild', false)
-
-    sh "docker build -t ${image}:${tag} ."
-
-    if (!onlyBuild) {
+def call(Map config = [:]) {
+    def image = config.image ?: 'default-image'
+    def tag = config.tag ?: 'latest'
+    def onlyPush = config.onlyPush ?: false
+    
+    if (!onlyPush) {
+        sh "docker build -t ${image}:${tag} ."
+    }
+    
+    if (!config.onlyBuild) {
         withCredentials([usernamePassword(
             credentialsId: 'dockerhub-credentials-id',
             usernameVariable: 'DOCKER_USER',
             passwordVariable: 'DOCKER_PASS'
         )]) {
-            sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
+            sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
             sh "docker push ${image}:${tag}"
         }
     }
