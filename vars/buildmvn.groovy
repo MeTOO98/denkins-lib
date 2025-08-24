d798e851a1e31b389fbb060c542b1b33b694d4b6
metoo <mohamedmetoo@gmail.com>
2025-08-24 22:25:03 +0300

    new jenkins-script file

--- vars/buildmvn.groovy
+++ vars/buildmvn.groovy
@@ -1,3 +1,7 @@
-def call() {
-    sh "mvn clean package -DskipTests"
+def call(Map args = [:]) {
+    if (args.skipTests) {
+        sh "mvn clean package -DskipTests"
+    } else {
+        sh "mvn clean package"
+    }
 }
